cd4135ef9674f0edc9f9d6f3dae5ff5b50646129
sckohl <sckohl@gmail.com>
2019-12-14 11:41:12 -0800

    can display a user's title

--- src/app/form-display/form-display.component.ts
+++ src/app/form-display/form-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
+import { stringify } from 'querystring';
 
 
 @Component({
@@ -15,13 +16,24 @@ url = "http://jsonplaceholder.typicode.com/posts/"
   
   constructor(private http: HttpClient ) {
 }
-
-
+/*
+lesson: you don't need the input variable to be 
+  declared in the Angular component. Just pass it in 
+  from the HTML component as a template variable.
+*/
 buttonclick(textVariable){
-    this.http.get(this.url + textVariable).subscribe(response => { console.log(response)});
+    this.http.get(this.url + textVariable).subscribe(response =>  {this.post = response; console.log(this.post.userId + this.post.title) });
+    
+      // { 
+      // this.post = response,
+      // console.log(response)});
   //  alert('Button was clicked')
-  return console.log(textVariable)
+    
   };
+
+a(){
+return console.log("second method a() "+this.post)
+}
 
   
 
